094fbb83e76d7d3a276ed2052e472fdc3fd0d808
jellywilly420 <65194730+jellywilly420@users.noreply.github.com>
2023-12-02 05:28:57 +0200

    Add function to add players to the game
    Add function to keep track of player turns and change turns
    
    Add function to clear the board

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
-function createGame() {
+const game = (function () {
   const gameBoard = [
     ["O", "X", "X"],
     ["X", "O", "O"],
     ["O", "X", "X"],
   ];
+
+  let players = [];
+
   const printBoard = () => {
     for (const row of gameBoard) {
       console.log(row);
     }
   };
+
   const isFull = () => {
     return (
       !gameBoard[0].includes("-") &&
@@ -16,6 +20,7 @@ function createGame() {
       !gameBoard[2].includes("-")
     );
   };
+
   //   returns winning marker if it exists
   const evaluate = () => {
     for (let i = 0; i < 3; i++) {
@@ -53,32 +58,86 @@ function createGame() {
     }
     return null;
   };
-  const placeMarker = (player, xCoord, yCoord) => {
-    if (gameBoard[xCoord][yCoord] === "-") {
-      gameBoard[xCoord][yCoord] = player.marker;
+
+  const logResult = () => {
+    if (playerX.isPlayerMarker(game.evaluate())) {
+      console.log("Player X wins!");
+    } else if (playerO.isPlayerMarker(game.evaluate())) {
+      console.log("Player O wins!");
+    } else if (game.evaluate() === "draw") {
+      console.log(game.evaluate());
     }
   };
-  return { placeMarker, printBoard, evaluate, isFull };
-}
+
+  const placeMarker = (xCoord, yCoord) => {
+    if (gameBoard[xCoord][yCoord] === "-") {
+      gameBoard[xCoord][yCoord] = turn.get().marker;
+    }
+  };
+
+  const addPlayer = (player) => {
+    players.push(player);
+  };
+
+  const turn = (function () {
+    let counter = 0;
+    // returns the player whose turn it is. always starts the game with x
+    const get = function () {
+      if (counter === 0) {
+        return players.filter((player) => {
+          return player.isPlayerMarker("X");
+        })[0];
+      } else if (counter % 2 === 1) {
+        return players.filter((player) => {
+          return player.isPlayerMarker("O");
+        })[0];
+      } else {
+        return players.filter((player) => {
+          return player.isPlayerMarker("X");
+        })[0];
+      }
+    };
+    // changes the player of the turn
+    const change = function () {
+      counter++;
+      console.log(`It's player ${game.turn.get().marker}'s turn.`);
+    };
+    return { get, change };
+  })();
+
+  const clearBoard = function () {
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        gameBoard[i][j] = "-";
+      }
+    }
+  };
+
+  return {
+    placeMarker,
+    printBoard,
+    evaluate,
+    isFull,
+    logResult,
+    addPlayer,
+    clearBoard,
+    players,
+    turn,
+  };
+})();
 
 function createPlayer(letter) {
   const marker = letter;
   const isPlayerMarker = (x) => {
     return marker === x;
   };
+
   return { marker, isPlayerMarker };
 }
 
-function gameController() {
-  const game = createGame();
+(function gameController() {
   const playerX = createPlayer("X");
   const playerO = createPlayer("O");
-  if (playerX.isPlayerMarker(game.evaluate())) {
-    console.log("Player X wins!");
-  } else if (playerO.isPlayerMarker(game.evaluate())) {
-    console.log("Player O wins!");
-  } else if (game.evaluate() === "draw") {
-    console.log(game.evaluate());
-  }
-}
-gameController();
+  game.addPlayer(playerX);
+  game.addPlayer(playerO);
+})();
